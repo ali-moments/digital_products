ac04dcc15b6917d43bf85d81e124b95abd209228
ali-moments <a.ham2334@gmail.com>
2023-09-04 11:47:24 +0330

    add template for local_settings.py

---
+++ digital_products/local_settings_template.py
@@ -0,0 +1,11 @@
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ''
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+IS_DEVEL = True
+
+ALLOWED_HOSTS = []
